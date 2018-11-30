674cba3b30743166bb02424e716d8f97034a422a
Arunkumar <araaru0@gmail.com>
2018-11-30 12:32:26 +0530

    registerscreen

--- registeractivity.java
+++ registeractivity.java
@@ -7,11 +7,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class registeractivity extends AppCompatActivity {
-        TextView tv =(TextView) findViewById(R.id.signup);
+      private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeractivity);
+        tv  =(TextView) findViewById(R.id.signup);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- signinactivity.java
+++ signinactivity.java
@@ -19,58 +19,66 @@ import org.json.JSONObject;
 
 
 public class signinactivity extends AppCompatActivity {
-    TextView signin = (TextView) findViewById(R.id.login);
-    EditText referralid = (EditText) findViewById(R.id.referralid);
-    EditText phone = (EditText) findViewById(R.id.phone) ;
-    EditText email = (EditText) findViewById(R.id.email) ;
-    EditText username= (EditText) findViewById( R.id.username) ;
-    EditText password = (EditText) findViewById(R.id.password) ;
-    Button register = (Button) findViewById(R.id.register);
-    String url= "https://arundemo1.herokuapp.com/";
-    final RequestQueue queue = Volley.newRequestQueue(this);
+    TextView signin;
+    EditText referralid;
+    EditText phone;
+    EditText email;
+    EditText username;
+    EditText password;
+    Button register;
+    String url = "https://arundemo1.herokuapp.com/";
+//    final RequestQueue queue = Volley.newRequestQueue(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signinactivity);
+        signin = (TextView) findViewById(R.id.login);
+        referralid = (EditText) findViewById(R.id.referralid);
+        phone = (EditText) findViewById(R.id.phone);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        username = (EditText) findViewById(R.id.username);
+        register = (Button) findViewById(R.id.register);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signin = new Intent(signinactivity.this,registeractivity.class);
+                Intent signin = new Intent(signinactivity.this, registeractivity.class);
                 startActivity(signin);
             }
         });
-        String refid = referralid.getText().toString();
-        String ph = phone.getText().toString();
-        String mail = email.getText().toString();
-        String uname = username.getText().toString();
-        String pass = password.getText().toString();
-            JSONObject json = new JSONObject();
-            try{
-                json.put("name",uname);
-                json.put("phone",ph);
-                json.put("email",mail);
-                json.put("referal",refid);
-                json.put("password",pass);
+//        String refid = referralid.getText().toString();
+//        String ph = phone.getText().toString();
+//        String mail = email.getText().toString();
+//        String uname = username.getText().toString();
+//        String pass = password.getText().toString();
+//            JSONObject json = new JSONObject();
+//            try{
+//                json.put("name",uname);
+//                json.put("phone",ph);
+//                json.put("email",mail);
+//                json.put("referal",refid);
+//                json.put("password",pass);
+//
+//            }catch (JSONException e)
+//            {
+//                e.printStackTrace();
+//            }
+//            final String JSONString = json.toString();
+//
+//    register.setOnClickListener(new View.OnClickListener() {
+//    @Override
+//    public void onClick(View v) {
+//        SendDatatoServer(JSONString);
+//    }
+//});
+//
+//}
+//    private void SendDatatoServer(final String json) {
+//
+//    }
 
-            }catch (JSONException e)
-            {
-                e.printStackTrace();
-            }
-            final String JSONString = json.toString();
 
-    register.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        SendDatatoServer(JSONString);
     }
-});
 
 }
-    private void SendDatatoServer(final String json) {
-
-    }
-
-
-}
-
